efe1e364a73f62558a16bc2eae19ea19c1c0b1da
Spencer Lee <spencerlee@m3.mynetworksettings.com>
2024-05-03 20:10:51 -0400

    Animated header, title padding

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -10,7 +10,16 @@ tailwind.config = {
       fontFamily: {
         'sans': ['Quando', 'sans-serif'],
         'serif': ['Judson', 'serif'],
-      }
+      },
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(10px)' },
+        },
+      },
     }
   }
 }
